5316292cd2dfe3748b090ba42e77a3ef0458c18f
hhuiminc <hhuiminc@gmail.com>
2025-05-03 21:01:49 +0800

    missed out footer links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,8 +7,8 @@ const Footer = () => {
       <small>
         <ul className="permalinks">
           <li><a href="#">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#experience">Experience</a></li>
+          {/* <li><a href="#about">About</a></li> */}
+          {/* <li><a href="#experience">Experience</a></li> */}
           <li><a href="#portfolio">Portfolio</a></li>
           <li><a href="#testimonials">Testimonials</a></li>
           <li><a href="#contact">Contact</a></li>
@@ -18,7 +18,7 @@ const Footer = () => {
 
 
       <div className="footer__copyright">
-        <small>&copy; <a href="#" className="footer__logo">HUIMINFOLIO</a> &#124; October 2022 
+        <small>&copy; <a href="#" className="footer__logo">HUIMINFOLIO</a> &#124; 2025 
           {/* <a href="https://www.flaticon.com/free-icons/think" title="think icons">Flaticon Icon</a> */}
           </small>
       </div>
