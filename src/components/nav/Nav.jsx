52ea64a71ea897621438253bf1e71086131a1917
hhuiminc <hhuiminc@gmail.com>
2025-05-03 20:05:00 +0800

    nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useState} from 'react'
 import {BiHomeAlt, BiUser, BiBookmarkAlt} from 'react-icons/bi'
+import { RiChat3Line } from "react-icons/ri";
 import {CgBriefcase} from 'react-icons/cg'
 import {FiPhone} from 'react-icons/fi'
 import './nav.css'
@@ -16,6 +17,7 @@ const Nav = () => {
       {/* <a href="#about" onClick={() => setActiveNav('#about')} className={"about " + (activeNav === '#about' ? 'active' : '')}><BiUser/></a> */}
       {/* <a href="#experience" onClick={() => setActiveNav('#experience')} className={"experience " + (activeNav === '#experience' ? 'active' : '')}><CgBriefcase/></a> */}
       <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={"portfolio " + (activeNav === '#portfolio' ? 'active' : '')}><BiBookmarkAlt/></a>
+      <a href="#testimonials" onClick={() => setActiveNav('#testimonials')} className={"testimonials " + (activeNav === '#testimonials' ? 'active' : '')}><RiChat3Line/></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={"contact " + (activeNav === '#contact' ? 'active' : '')}><FiPhone/></a>
     </nav>
   )
